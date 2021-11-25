1bf8d55330bafa6dbf6440dd946daa521f138953
Jonas. L <33558637+Eksno@users.noreply.github.com>
2021-11-25 13:27:49 +0100

    something is wrong about requirements

--- app/views.py
+++ app/views.py
@@ -1,8 +1,30 @@
 from app import app
-from flask import render_template
+from flask import render_template, jsonify
+
+import os
+import psycopg2
 
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template("home.html")
+
+@app.route("/db")
+def db():
+    conn = psycopg2.connect(os.environ.get('DATABASE_URL', 'postgres://postgres:postgres@localhost:5432/postgres'))
+
+    cur = conn.cursor()
+    
+    try:
+        cur.execute('''SELECT * FROM cumulative''')
+        row_headers=[x[0] for x in cur.description]  # this will extract row headers
+        rv = cur.fetchall()
+        
+        json_data=[]
+        for result in rv:
+            json_data.append(dict(zip(row_headers,result)))
+        
+        return jsonify(json_data)
+    except:
+        return "Table probably not made yet"
