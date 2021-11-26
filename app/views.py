09ec956a69cc8e35e84bc4d09beae9290047f2b4
Jonas. L <33558637+Eksno@users.noreply.github.com>
2021-11-26 10:43:37 +0100

    logging

--- app/views.py
+++ app/views.py
@@ -2,24 +2,27 @@ from app import app
 from flask import render_template, jsonify
 
 import os
+import logging
 import psycopg2
 
 
 @app.route("/")
 @app.route("/home")
 def home():
+    logging.info("/ or /home loaded")
     return render_template("home.html")
 
 
 @app.route("/db")
 def db():
+    logging.info("/db loaded")
     conn = psycopg2.connect(
         os.environ.get("DATABASE_URL", "postgres://postgres:postgres@db:5432/postgres")
     )
+    logging.debug("connected to database")
 
-    cur = conn.cursor()
-
-    with conn.cursor as cur:
+    with conn.cursor() as cur:
+        logging.debug("opened cursor")
         cur.execute(
             """
             SELECT
@@ -31,23 +34,26 @@ def db():
 
         row_headers = [x[0] for x in cur.description]  # this will extract row headers
         rv = cur.fetchall()
+    logging.debug("exited cursor")
 
     json_data = []
     for result in rv:
         json_data.append(dict(zip(row_headers, result)))
+    logging.debug("created json data")
 
     return jsonify(json_data)
 
 
 @app.route("/db/koic")
 def db_koi_disposition_confirmed():
+    logging.info("/db/koic loaded")
     conn = psycopg2.connect(
         os.environ.get("DATABASE_URL", "postgres://postgres:postgres@db:5432/postgres")
     )
-
-    cur = conn.cursor()
+    logging.debug("connected to database")
 
     with conn.cursor() as cur:
+        logging.debug("opened cursor")
         cur.execute(
             """
             SELECT
@@ -61,21 +67,26 @@ def db_koi_disposition_confirmed():
 
         row_headers = [x[0] for x in cur.description]  # this will extract row headers
         rv = cur.fetchall()
+    logging.debug("exited cursor")
 
     json_data = []
     for result in rv:
         json_data.append(dict(zip(row_headers, result)))
+    logging.debug("created json data")
 
     return jsonify(json_data)
 
 
 @app.route("/db/<int:kepid>")
 def db_kepid(kepid):
+    logging.info(f"/db/{kepid} loaded")
     conn = psycopg2.connect(
         os.environ.get("DATABASE_URL", "postgres://postgres:postgres@db:5432/postgres")
     )
+    logging.debug("connected to database")
 
     with conn.cursor() as cur:
+        logging.debug("opened cursor")
         cur.execute(
             """
             SELECT
@@ -90,9 +101,11 @@ def db_kepid(kepid):
 
         row_headers = [x[0] for x in cur.description]  # this will extract row headers
         rv = cur.fetchall()
+    logging.debug("exited cursor")
 
     json_data = []
     for result in rv:
         json_data.append(dict(zip(row_headers, result)))
+    logging.debug("created json data")
 
     return jsonify(json_data)
