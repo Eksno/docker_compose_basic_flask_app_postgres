f056de0956528792731cb4a0bafbf38aab8a255b
Jonas. L <33558637+Eksno@users.noreply.github.com>
2021-11-26 12:30:42 +0100

    add metrics using prometheus

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from waitress import serve
 import logging
 
 if __name__ == "__main__":
-    logging.basicConfig(filename="app.log", filemode="w", level=logging.INFO)
+    logging.basicConfig(filename="app.log", filemode="w", level=logging.DEBUG)
     serve(app, host="0.0.0.0", port=8080, url_scheme="https")
