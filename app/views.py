9630095719911bd7af2bf4dc1b261d2613f0d0fe
Jonas. L <33558637+Eksno@users.noreply.github.com>
2021-11-26 10:17:36 +0100

    security updates and new endpoints

--- app/views.py
+++ app/views.py
@@ -10,21 +10,89 @@ import psycopg2
 def home():
     return render_template("home.html")
 
+
 @app.route("/db")
 def db():
-    conn = psycopg2.connect(os.environ.get('DATABASE_URL', 'postgres://postgres:postgres@db:5432/postgres'))
+    conn = psycopg2.connect(
+        os.environ.get("DATABASE_URL", "postgres://postgres:postgres@db:5432/postgres")
+    )
 
     cur = conn.cursor()
-    
-    try:
-        cur.execute('''SELECT * FROM cumulative''')
-        row_headers=[x[0] for x in cur.description]  # this will extract row headers
+
+    with conn.cursor as cur:
+        cur.execute(
+            """
+            SELECT
+                *
+            FROM
+                cumulative
+            """
+        )
+
+        row_headers = [x[0] for x in cur.description]  # this will extract row headers
         rv = cur.fetchall()
-        
-        json_data=[]
-        for result in rv:
-            json_data.append(dict(zip(row_headers,result)))
-        
-        return jsonify(json_data)
-    except:
-        return "Table probably not made yet"
+
+    json_data = []
+    for result in rv:
+        json_data.append(dict(zip(row_headers, result)))
+
+    return jsonify(json_data)
+
+
+@app.route("/db/koic")
+def db_koi_disposition_confirmed():
+    conn = psycopg2.connect(
+        os.environ.get("DATABASE_URL", "postgres://postgres:postgres@db:5432/postgres")
+    )
+
+    cur = conn.cursor()
+
+    with conn.cursor() as cur:
+        cur.execute(
+            """
+            SELECT
+                *
+            FROM
+                cumulative
+            WHERE
+                koi_disposition = 'CONFIRMED'
+            """
+        )
+
+        row_headers = [x[0] for x in cur.description]  # this will extract row headers
+        rv = cur.fetchall()
+
+    json_data = []
+    for result in rv:
+        json_data.append(dict(zip(row_headers, result)))
+
+    return jsonify(json_data)
+
+
+@app.route("/db/<int:kepid>")
+def db_kepid(kepid):
+    conn = psycopg2.connect(
+        os.environ.get("DATABASE_URL", "postgres://postgres:postgres@db:5432/postgres")
+    )
+
+    with conn.cursor() as cur:
+        cur.execute(
+            """
+            SELECT
+                *
+            FROM
+                cumulative
+            WHERE
+                kepid = %(kepid)s
+            """,
+            {"kepid": kepid},
+        )
+
+        row_headers = [x[0] for x in cur.description]  # this will extract row headers
+        rv = cur.fetchall()
+
+    json_data = []
+    for result in rv:
+        json_data.append(dict(zip(row_headers, result)))
+
+    return jsonify(json_data)
